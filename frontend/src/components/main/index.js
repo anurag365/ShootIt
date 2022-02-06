972d492c033e4b2d7175853a13a9b94ba795e1b4
anuragp067@gmail.com <anuragp067@gmail.com>
2022-02-06 19:31:20 +0530

    ..

--- frontend/src/components/main/index.js
+++ frontend/src/components/main/index.js
@@ -1,15 +1,11 @@
-import { Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
-
-const Main =() =>{
-
-
-    return (
-        <>
-        
-        <h1>Main</h1>
-        <Outlet/>
-        </>
-    );
+const Main = () => {
+  return (
+    <>
+      <h1>Main</h1>
+      <Outlet />
+    </>
+  );
 };
 export default Main;
