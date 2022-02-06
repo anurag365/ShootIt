7431b63f70abb3ceb08209201311425184aa71ca
anuragp067@gmail.com <anuragp067@gmail.com>
2022-02-06 19:26:30 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import Home from "./components/main/home";
+import Main from "./components/main";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          {<Route element={<Main />} path="main" />}
+          {<Route element={<Home />} path="home" />}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/components/main/index.js
@@ -0,0 +1,15 @@
+import { Outlet} from "react-router-dom";
+
+
+const Main =() =>{
+
+
+    return (
+        <>
+        
+        <h1>Main</h1>
+        <Outlet/>
+        </>
+    );
+};
+export default Main;
