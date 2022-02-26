98f7da49ede1aa26999b9263443da513b2e6d872
m-triple-m <triplem656@gmail.com>
2022-02-26 19:17:36 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Main from "./components/main";
 import Home from "./components/main/home";
 import SignUp from "./components/main/signup";
 import AddEquipment from "./components/admin/addEquipment";
 import Admin from "./components/admin";
+import Login from "./components/main/login";
 
 function App() {
   return (
@@ -13,12 +14,14 @@ function App() {
         <Routes>
           <Route element={<Main />} path="main">
             <Route element={<Home />} path="home" />
-            <Route element ={<SignUp/>} path ="signup"/>
-            <Route element ={<AddEquipment/>} path ="addEquiment"/>
+            <Route element={<SignUp />} path="signup" />
+            <Route element={<Login />} path="login" />
+            <Route element={<AddEquipment />} path="addEquiment" />
           </Route>
-          <Route element={<Admin />} path="admin">     
-          <Route element ={<AddEquipment/>} path ="addEquiment"/>
+          <Route element={<Admin />} path="admin">
+            <Route element={<AddEquipment />} path="addEquiment" />
           </Route>
+          <Route exact element={<Navigate to="/main/home" />} path="" />
         </Routes>
       </BrowserRouter>
     </div>
