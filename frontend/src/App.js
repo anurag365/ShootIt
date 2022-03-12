43f8f17b8ec43faaa3fc0418cda6b857f8487cdc
rajdubey05 <99rajdubey@gmail.com>
2022-03-12 19:57:29 +0530

    raj

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import AddEquipment from "./components/admin/addEquipment";
 import Admin from "./components/admin";
 import Login from "./components/main/login";
 import BrowseEquipment from "./components/main/browseEquipment";
+import ViewEqiupment from "./components/main/viewEqiupment";
 
 function App() {
   return (
@@ -17,10 +18,12 @@ function App() {
             <Route element={<Home />} path="home" />
             <Route element={<SignUp />} path="signup" />
             <Route element={<Login />} path="login" />
+            <Route element={<BrowseEquipment />} path="browseEquipment" />
+            <Route element={<ViewEqiupment />} path="viewEquipment" />
           </Route>
           <Route element={<Admin />} path="admin">
+            
             <Route element={<AddEquipment />} path="addEquipment" />
-            <Route element={<BrowseEquipment />} path="browseEquipment" />
           </Route>
           <Route exact element={<Navigate to="/main/home" />} path="" />
         </Routes>
