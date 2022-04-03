abbc203c3707d0431143c34fe36e721521141743
rishicastic <saumyesriv19@gmail.com>
2022-04-03 19:31:55 +0530

    ..

--- frontend/src/components/main/browseEquipment.js
+++ frontend/src/components/main/browseEquipment.js
@@ -1,95 +1,135 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Formik } from "formik";
-import { Button, Card, CardContent, TextField } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Paper,
+  Stack,
+  TextField,
+} from "@mui/material";
 import app_config from "../../config";
 import Swal from "sweetalert2";
+import { Link } from "react-router-dom";
 
 const BrowseEquipment = () => {
   const url = app_config.api_url;
-  const equipmentform = {
-    title: "",
-    tags: "",
-    thumbnail: "",
-    description: "",
-    rentableField: false,
-  };
 
-  const searchEquipment = (values) => {
-    fetch(url + "/equipment/add", {
-      method: "POST",
-      body: JSON.stringify(values),
-      headers: { "Content-Type": "application/json" },
-    })
+  const [equipmentData, setEquipmentData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchEquipment = () => {
+    fetch(url + "/equipment/getall")
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        Swal.fire({
-          icon: "success",
-          title: "Registered Successfully!!",
-        });
+        setEquipmentData(data);
+        setLoading(false);
       });
   };
 
-  return (
-    <div class="container mt-5">
-      <Card>
-        <CardContent>
-          <div class="card">
-            <div class="row">
-              <div class="col-md">
-                <div class="img-back"></div>
-              </div>
+  // const newCont =()=>{
+  //   return(
+  //     <Grid container spacing ={5}>
+  //     <Grid item md={8}>
+  //     <Stack spacing={2} direction="row">
+  //     <Button variant="contained">Camera</Button>
+  //     <Button variant="contained">Lens</Button>
+  //     <Button variant="contained">Microphone</Button>
+  //   </Stack>
+  //     </Grid>
 
-              <div class="col-md">
-                <div class="card-body my-card-body">
-                  <p class="h3">Search Equipment</p>
-                  <p class="text-muted">Search equipment here</p>
+  //     </Grid>
 
-                  <hr />
+  //   );
 
-                  <Formik
-                    initialValues={equipmentform}
-                    onSubmit={searchEquipment}
+  // };
+
+  const displayEquipment = () => {
+    if (!loading) {
+      return (
+        <Grid container spacing={5}>
+          {equipmentData.map((equipment) => (
+            <Grid item md={4}>
+              <Card>
+                <CardMedia
+                  component="img"
+                  image={url + "/uploads/" + equipment.thumbnail}
+                  height="200"
+                />
+                <CardContent>
+                  <h4>{equipment.title}</h4>
+                  <p>{equipment.description}</p>
+                  <Link
+                    to={"/view/" + equipment._id}
+                    component={Button}
+                    variant="contained"
+                    color="primary"
                   >
-                    {({ values, handleSubmit, handleChange }) => (
-                      <form onSubmit={handleSubmit}>
-                        <TextField
-                          className="w-100 mt-5"
-                          variant="filled"
-                          id="title"
-                          type="text"
-                          label="Equipment Name"
-                          onChange={handleChange}
-                          value={values.title}
-                        />
+                    View Equipment
+                  </Link>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      );
+    }
+  };
 
-                        <TextField
-                          className="w-100 mt-3"
-                          id="type"
-                          type="text"
-                          label="Type"
-                          onChange={handleChange}
-                          value={values.tags}
-                        ></TextField>
+  const showEquipmentGrid = () => {
+    return (
+      <Container>
+        <Grid container>
+          <Grid item md={4}>
+            <Card>
+              <CardMedia
+                component="img"
+                image={url + "/images/Canon.png"}
+              ></CardMedia>
+              <CardContent>
+                <h2>Canon M50</h2>
+                <p>Nice Camera</p>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    );
+  };
 
-                        <Button
-                          type="submit"
-                          color="primary"
-                          variant="contained"
-                          className="mt-5 w-50"
-                        >
-                          Search Equipment
-                        </Button>
-                      </form>
-                    )}
-                  </Formik>
-                </div>
-              </div>
-            </div>
-          </div>
-        </CardContent>
-      </Card>
-    </div>
+  useEffect(() => {
+    fetchEquipment();
+  }, []);
+
+  return (
+    <Paper style={{ height: "100vh" }}>
+      <header className="bg-warning">
+        <Container>
+          <h1>Browse Equipment</h1>
+          <TextField
+            label="Search Equipment"
+            variant="outlined"
+            className="w-100"
+          />
+              <Stack spacing={2} direction="row">
+            <Button variant="contained">Camera</Button>
+            <Button variant="contained">Lens</Button>
+            <Button variant="contained">Microphone</Button>
+          </Stack>
+        </Container>
+      </header>
+
+      <Container>
+        <Grid item md={3}></Grid>
+        <Grid item md={9}>
+          {displayEquipment()}
+        </Grid>
+      </Container>
+    </Paper>
   );
 };
 
